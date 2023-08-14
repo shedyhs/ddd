22e224209e6e0b8a1ca004ec85123a4ce105fb22
shedy <shedyhs@gmail.com>
2023-08-14 15:00:07 -0400

    implements transactions for unit of work

--- src/@core/common/infra/unit-of-work-mikro-orm.ts
+++ src/@core/common/infra/unit-of-work-mikro-orm.ts
@@ -3,9 +3,26 @@ import { IUnitOfWork } from '../application/unit-of-work.interface';
 
 export class UnitOfWorkMikroOrm implements IUnitOfWork {
   constructor(private entityManager: EntityManager) {}
+
+  async beginTransaction(): Promise<void> {
+    return this.entityManager.begin();
+  }
+
+  async completeTransaction(): Promise<void> {
+    return this.entityManager.commit();
+  }
+
+  async rollbackTransaction(): Promise<void> {
+    this.entityManager.rollback();
+  }
+  async runTransaction<T>(callback: () => Promise<T>): Promise<T> {
+    return this.entityManager.transactional(callback)
+  }
+
   async commit(): Promise<void> {
     return this.entityManager.flush();
   }
+
   async rollback(): Promise<void> {
     this.entityManager.clear();
   }
