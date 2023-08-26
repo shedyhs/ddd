83ebb226b1b43a1d8587f6c5eb2f3193e0ad6eda
shedy <shedyhs@gmail.com>
2023-08-26 15:34:54 -0400

    fix initial migration

--- migrations/Migration20230826191422.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20230826191422 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('select 1');
-  }
-
-}
